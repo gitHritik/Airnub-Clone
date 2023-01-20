95eccfb529eec15dfbe84079cd676f209e5cdbe4
gitHritik <97736648+gitHritik@users.noreply.github.com>
2023-01-20 17:59:42 +0530

    Done

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-600">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airnub works</p>
+        <p>NewRoom</p>
+        <p>Investor</p>
+        <p>Airnub Plus</p>
+        <p>Airnub Xur</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-600">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airnub works</p>
+        <p>NewRoom</p>
+        <p>Investor</p>
+        <p>Airnub Plus</p>
+        <p>Airnub Xur</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-600">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airnub works</p>
+        <p>NewRoom</p>
+        <p>Investor</p>
+        <p>Airnub Plus</p>
+        <p>Airnub Xur</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-600">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airnub works</p>
+        <p>NewRoom</p>
+        <p>Investor</p>
+        <p>Airnub Plus</p>
+        <p>Airnub Xur</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
